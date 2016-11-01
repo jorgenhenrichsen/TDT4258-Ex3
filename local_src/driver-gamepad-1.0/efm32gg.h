ad3478494d8b06355cb6fae0b3706825aa4d6312
jorgenhenrichsen <jh.henrichs@gmail.com>
2016-11-01 16:51:35 +0100

    Interrupt handling

--- local_src/driver-gamepad-1.0/efm32gg.h
+++ local_src/driver-gamepad-1.0/efm32gg.h
@@ -42,7 +42,7 @@
 #define GPIO_EXTIFALL  ((volatile uint32_t*)(GPIO_PA_BASE + 0x10c))
 #define GPIO_IEN       ((volatile uint32_t*)(GPIO_PA_BASE + 0x110))
 #define GPIO_IFC       ((volatile uint32_t*)(GPIO_PA_BASE + 0x11c))
-
+#define GPIO_IF        ((volatile uint32_t*)(GPIO_PA_BASE + 0x114))
 // CMU
 
 #define CMU_BASE2 0x400c8000
@@ -140,4 +140,3 @@
 #define SCR          ((volatile uint32_t*)0xe000ed10)
 #define SYSTICK_CTRL ((volatile uint32_t*)0xe000e010)
 #define SYSTICK_LOAD ((volatile uint32_t*)0xe000e014)
-
