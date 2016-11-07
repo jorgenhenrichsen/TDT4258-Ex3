2563cc1be186e708f05fd9418c965195cdea7d30
jorgenhenrichsen <jh.henrichs@gmail.com>
2016-11-07 15:38:36 +0100

    Game receives sigio

--- local_src/driver-gamepad-1.0/driver-gamepad.c
+++ local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -15,6 +15,8 @@
 #include <asm/uaccess.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <asm/signal.h>
+#include <asm/siginfo.h>
 
 /*
 * template_init - function to insert this module into kernel space
@@ -29,7 +31,7 @@
 #define Button2 1 << 1
 #define Button3 1 << 2
 #define Button4 1 << 3
-#define DRIVER_NAME "Gamepad_Driver"
+#define DRIVER_NAME "gamepad"
 #define GPIO_EVEN_IRQ 17
 #define GPIO_ODD_IRQ 18
 
@@ -38,6 +40,7 @@ static int release_gamepad(struct inode* inode, struct file* filp);
 static ssize_t read_gamepad(struct file* filp, char* __user buff, size_t count, loff_t* offp);
 static ssize_t write_gamepad(struct file *filp, const char __user *buff, size_t count, loff_t *offp);
 static irqreturn_t gpio_interrupt_handler(int irq, void * dev_id, struct pt_regs* regs);
+static int fasync_gamepad(int fd, struct file *filp, int on);
 
 dev_t devno;
 int count;
@@ -45,65 +48,67 @@ struct class *cl;
 //static unsigned int button1 = *GPIO_PC_DIN;
 
 
+static struct fasync_struct *gamepad_fasync = NULL;
 
 static struct file_operations gdriv_fops = {
  .owner = THIS_MODULE,
  .open = open_gamepad,
  .release = release_gamepad,
  .read = read_gamepad,
- .write = write_gamepad
+ .write = write_gamepad,
+ .fasync = fasync_gamepad
 };
 
 struct cdev gamepad_cdev;
 
 static int __init template_init(void)
 {
-printk("Gamepad driver init \n");
+  printk("Gamepad driver init \n");
 
-devno = MKDEV(15, 20);
-count = 5;
+  devno = MKDEV(15, 20);
+  count = 5;
 
-// TODO: Use alloc_chrdev instead.
-int status = register_chrdev_region(devno, count, DRIVER_NAME);
+  // TODO: Use alloc_chrdev instead.
+  int status = register_chrdev_region(devno, count, DRIVER_NAME);
 
-if (status < 0){
-	printk("ERROR registering chrdev region: STATUSCODE: %d \n", status);
-}
-else {
-	printk("Reigstered chrdev region \n");
-}
+  if (status < 0){
+    printk("ERROR registering chrdev region: STATUSCODE: %d \n", status);
+  }
+  else {
+  	printk("Reigstered chrdev region \n");
+  }
 
-if (request_mem_region(GPIO_PC_DIN, 1, DRIVER_NAME) == NULL){
-	printk(KERN_ALERT "Error requesting GPIO_PC_DIN region \n");
-}
-if (request_mem_region(GPIO_PC_DOUT, 1, DRIVER_NAME) == NULL){
-	printk(KERN_ALERT "Error requesting GPIO_PC_DOUT region \n");
-}
-if (request_mem_region(GPIO_PC_MODEL, 1, DRIVER_NAME) == NULL){
-	printk(KERN_ALERT "Error requesting GPIO_PC_MODEL region \n");
-}
+  if (request_mem_region(GPIO_PC_DIN, 1, DRIVER_NAME) == NULL){
+  	printk(KERN_ALERT "Error requesting GPIO_PC_DIN region \n");
+  }
+  if (request_mem_region(GPIO_PC_DOUT, 1, DRIVER_NAME) == NULL){
+  	printk(KERN_ALERT "Error requesting GPIO_PC_DOUT region \n");
+  }
+  if (request_mem_region(GPIO_PC_MODEL, 1, DRIVER_NAME) == NULL){
+  	printk(KERN_ALERT "Error requesting GPIO_PC_MODEL region \n");
+  }
 
-iowrite32(0x33333333, GPIO_PC_MODEL); // Set pin 0-7 as input.
-iowrite32(0xFF, GPIO_PC_DOUT); // Enable internal pull-up.
+  iowrite32(0x33333333, GPIO_PC_MODEL); // Set pin 0-7 as input.
+  iowrite32(0xFF, GPIO_PC_DOUT); // Enable internal pull-up.
 
-/*Setup interrupts*/
-iowrite32(0x2222222, GPIO_EXTIPSELL); // Enable interrupts.
-request_irq(GPIO_EVEN_IRQ, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &gamepad_cdev);
-request_irq(GPIO_ODD_IRQ, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &gamepad_cdev);
+  /*Setup interrupts*/
+  iowrite32(0x2222222, GPIO_EXTIPSELL); // Enable interrupts.
+  request_irq(GPIO_EVEN_IRQ, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &gamepad_cdev);
+  request_irq(GPIO_ODD_IRQ, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &gamepad_cdev);
 
-/*Enable iterrupts*/
-iowrite32(0xFF, GPIO_EXTIFALL); // Enable interrupts on fall.
-iowrite32(0xFF, GPIO_IEN); // Enable GPIO interrupt generation.
+  /* Enable iterrupts */
+  iowrite32(0xFF, GPIO_EXTIFALL); // Enable interrupts on fall.
+  iowrite32(0xFF, GPIO_IEN); // Enable GPIO interrupt generation.
 
-cdev_init(&gamepad_cdev, &gdriv_fops); // init our cdev stucture.
-gamepad_cdev.owner = THIS_MODULE; // Set the cdev owner.
-cdev_add(&gamepad_cdev, devno, count); // Tell kernel about our cdev.
+  cdev_init(&gamepad_cdev, &gdriv_fops); // init our cdev stucture.
+  gamepad_cdev.owner = THIS_MODULE; // Set the cdev owner.
+  cdev_add(&gamepad_cdev, devno, count); // Tell kernel about our cdev.
 
-// Make the driver appear as a file in /dev.
-cl = class_create(THIS_MODULE, DRIVER_NAME); //
-device_create(cl, NULL, devno, NULL, DRIVER_NAME);
+  // Make the driver appear as a file in /dev.
+  cl = class_create(THIS_MODULE, DRIVER_NAME); //
+  device_create(cl, NULL, devno, NULL, DRIVER_NAME);
 
-return 0;
+  return 0;
 }
 
 /*
@@ -121,45 +126,54 @@ static void __exit template_cleanup(void)
 
 }
 
+static int fasync_gamepad(int fd, struct file *filp, int on)
+{
+  int retval = fasync_helper(fd, filp, on, &gamepad_fasync);
+
+  if (retval < 0)
+    return retval;
+  return 0;
+}
+
 
 irqreturn_t gpio_interrupt_handler(int irq, void * dev_id, struct pt_regs* regs){
 
 	iowrite32(ioread32(GPIO_IF), GPIO_IFC); // Clear interrupt flag.
 
-	int buttonPressed = (uint8_t)~(ioread32(GPIO_PC_DIN));
+	/*int buttonPressed = (uint8_t)~(ioread32(GPIO_PC_DIN));
 	switch (buttonPressed) {
 		case Button1: printk("Button1 \n"); break;
 		case Button2: printk("Button2 \n"); break;
 		case Button3: printk("Button3 \n"); break;
 		case Button4: printk("Button4 \n"); break;
-		default: return;
-	}
+		default: return IRQ_HANDLED;
+	}*/
 
-
-
+  if (gamepad_fasync){
+    kill_fasync(&gamepad_fasync, SIGIO, POLL_IN);
+  }
 	return IRQ_HANDLED;
 }
 
 static int open_gamepad(struct inode* inode, struct file* filp){
-printk(KERN_INFO "Opening gamepad driver.\n");
-return 0;
+  printk(KERN_INFO "Opening gamepad driver.\n");
+  return 0;
 }
 
 static int release_gamepad(struct inode* inode, struct file* filp){
-printk(KERN_INFO "Releasing gamepad driver.\n");
-return 0;
+  printk(KERN_INFO "Releasing gamepad driver.\n");
+  return 0;
 }
 
 static ssize_t read_gamepad(struct file* filp, char* __user buff, size_t count, loff_t* offp){
-uint32_t data = ioread32(GPIO_PC_DIN);
-printk("%d", data);
-copy_to_user(buff, &data, 1);
-return 1;
+  uint32_t data = ioread32(GPIO_PC_DIN);
+  copy_to_user(buff, &data, 1);
+  return 1;
 }
 
 static ssize_t write_gamepad(struct file *filp, const char __user *buff, size_t count, loff_t *offp){
-printk(KERN_INFO "Don't write to the buttons you fools!\n");
-return 1;
+  printk(KERN_INFO "Don't write to the buttons you fools!\n");
+  return 1;
 }
 
 
--- local_src/game-1.0/game.c
+++ local_src/game-1.0/game.c
@@ -1,9 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/fcntl.h>
+#include <linux/fs.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <linux/fb.h>
+
+FILE* gamepad;
+FILE* fb;
+
+void sigio_handler(int signo);
+void getBinString(int value, char* output);
+void setupFramebuffer();
+void setupGamepad();
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
 
+	setupFramebuffer();
+	setupGamepad();
+
+	while (1) {
+		
+	}
+
 	exit(EXIT_SUCCESS);
 }
+
+
+void setupGamepad(){
+	gamepad = fopen("dev/gamepad", "rb");
+
+	if (!gamepad){
+		printf("Unable to open gamepad driver.\n");
+	}
+	
+	if (signal(SIGIO, &sigio_handler) == SIG_ERR) {
+		printf("Error occured with the signal handler.\n");
+	}
+	
+	if (fcntl(fileno(gamepad), F_SETOWN, getpid()) == -1) {
+		printf("Error setting owner\n");
+	}
+	
+	long oflags = fcntl(fileno(gamepad), F_GETFL);
+	if (fcntl(fileno(gamepad), F_SETFL, oflags | FASYNC) == -1) {
+		printf("Error setting FASYNC flag.\n");
+	}
+}
+
+void setupFramebuffer(){
+
+	fb = fopen("dev/fb0", O_RDWR);
+	
+	if (fb == -1){
+		printf("Unable to open lcd driver.\n");
+	}
+}
+
+
+void sigio_handler(int signo){
+	printf("Received signal: %d\n", signo);
+	int input = (uint8_t)~(fgetc(gamepad));
+	char binary[9];
+	getBinString(input, binary);
+	
+	int i;
+	for (i = 7; i >= 0; i--) {
+		if (binary[i] == '1'){
+			int button = 7 - i;
+			printf("Button%d pressed \n", button);
+		}
+	}
+	
+} 
+
+void getBinString(int value, char* output)
+{
+    int i;
+    output[8] = '\0';
+    for (i = 7; i >= 0; --i, value >>= 1)
+    {
+        output[i] = (value & 1) + '0';
+    }
+}
+
+
+
+
+
+
